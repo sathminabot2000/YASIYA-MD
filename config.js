ad6f48a6650fb46011c6d1ff8d79a5540c154544
sathminabot2000 <dewminapramudith@gmail.com>
2025-10-05 11:20:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~oMZkGSpL#wKCZtkQ0QsxsAnQqFl1eZmwPTxk0gwerWOxZ_hyvEj4'
 };
